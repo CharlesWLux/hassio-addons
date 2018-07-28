d437b45154c87ef32cb9f24a2dec2a83c7c2df7c
Chunhui Wang <wangchunhui.ufo@gmail.com>
2018-07-29 00:43:06 +0200

    test

--- samba/run.sh
+++ samba/run.sh
@@ -3,11 +3,12 @@ set -e
 
 # Mount drive
 echo "[Info] Mount drive /dev/sda1 ..."
-mount /dev/sda1 /share
+mount --bind /dev/sda1 /share
 echo "[Info] /dev/sda1 Done!!!"
 ls /share
 
 while true; do
     echo "[Info] heart beat!!!"
-    sleep 300
+    sleep 30
+    ls share
 done
